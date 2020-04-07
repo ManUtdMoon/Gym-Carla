4a4b39b92fa03c5cdc80bcc9a2f48237c76ea183
ManUtdMoon <yudongjie.moon@foxmail.com>
2020-04-07 13:27:45 +0800

    vectorized state, remove driving command

--- gym_carla/envs/carla_env.py
+++ gym_carla/envs/carla_env.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 # Copyright (c) 2020: Dongjie yu (yudongjie.moon@foxmail.com)
+
 # This file is modified from <https://github.com/cjy1992/gym-carla>:
 # Copyright (c) 2019:
 # author: Jianyu Chen (jianyuchen@berkeley.edu)
@@ -24,7 +25,6 @@ import carla
 import cv2
 
 from .coordinates import train_coordinates
-from .planner.planner import Planner
 from .misc import *
 from .carla_logger import *
 
@@ -65,8 +65,8 @@ class CarlaEnv(gym.Env):
         # action and observation space
         self.action_space = spaces.Box(np.array([-1.0, -1.0]),
             np.array([1.0, 1.0]), dtype=np.float32)
-        self.state_space = spaces.Box(low=0.0, high=1.0,
-            shape=(self.state_size[1], self.state_size[0], 3), dtype=np.float32)
+        self.state_space = spaces.Box(low=0.0, high=20.0,
+            shape=(8,), dtype=np.float32)
 
         # Connect to carla server and get world object
         # print('connecting to Carla server...')
@@ -80,16 +80,16 @@ class CarlaEnv(gym.Env):
         self.collision_hist_l = 1 # collision history length
         self.collision_bp = self.world.get_blueprint_library().find('sensor.other.collision')
 
-        # Add the bp of camera sensor
-        self.camera_img = np.zeros((self.obs_size[0], self.obs_size[1], 3), dtype=np.uint8)
-        self.camera_trans = carla.Transform(carla.Location(x=1.8, z=1.5), carla.Rotation(pitch=-10))
-        self.camera_bp = self.world.get_blueprint_library().find('sensor.camera.rgb')
-        # Modify the attributes of the blueprint to set image resolution and field of view.
-        self.camera_bp.set_attribute('image_size_x', str(self.obs_size[0]))
-        self.camera_bp.set_attribute('image_size_y', str(self.obs_size[1]))
-        self.camera_bp.set_attribute('fov', '110')
-        # Set the time in seconds between sensor captures
-        self.camera_bp.set_attribute('sensor_tick', '0.02')
+        # # Add the bp of camera sensor
+        # self.camera_img = np.zeros((self.obs_size[0], self.obs_size[1], 3), dtype=np.uint8)
+        # self.camera_trans = carla.Transform(carla.Location(x=1.8, z=1.5), carla.Rotation(pitch=-10))
+        # self.camera_bp = self.world.get_blueprint_library().find('sensor.camera.rgb')
+        # # Modify the attributes of the blueprint to set image resolution and field of view.
+        # self.camera_bp.set_attribute('image_size_x', str(self.obs_size[0]))
+        # self.camera_bp.set_attribute('image_size_y', str(self.obs_size[1]))
+        # self.camera_bp.set_attribute('fov', '110')
+        # # Set the time in seconds between sensor captures
+        # self.camera_bp.set_attribute('sensor_tick', '0.02')
 
         # Lane Invasion Sensor
         self.lane_bp = self.world.get_blueprint_library().find('sensor.other.lane_invasion')
@@ -109,6 +109,9 @@ class CarlaEnv(gym.Env):
         # A list stores the ids for each episode
         self.actors = []
 
+        # The tuple (x,y) for the current waypoint
+        self.current_wpt = np.array((self.start[0], self.start[1]))
+
 
     def step(self, action):
         # Assign acc/steer/brake to action signal
@@ -119,23 +122,19 @@ class CarlaEnv(gym.Env):
         else:
             throttle = 0
             brake = -throttle_or_brake
-        # print(action)
-        # print(self.ego.get_velocity())
-        # print(self.time_step)
+
         # Apply control
         act = carla.VehicleControl(throttle=float(throttle), steer=float(steer), brake=float(brake))
         self.ego.apply_control(act)
 
         self.world.tick()
 
-        # Route Planner
-        directions = self._get_directions(self.ego.get_transform(), self.dest)
-        self.last_direction = directions
-        # print("command is %s" % self.instruction[self.last_direction])
-
         # calculate reward
         ego_x, ego_y = self._get_ego_pos()
         self.new_dist = np.linalg.norm((ego_x-self.dest[0], ego_y-self.dest[1]))
+
+        # Get waypoint infomation
+        self.current_wpt = self._get_waypoint_xy()
 
         delta_yaw = self._get_delta_yaw()
         dyaw_dt = self.ego.get_angular_velocity().z
@@ -149,12 +148,12 @@ class CarlaEnv(gym.Env):
         self.v_t = np.array([[velocity.x], [velocity.y]])
         self.a_t = np.array([[accel.x], [accel.y]])
 
-        self.state_info['dist_to_dest'] = self.new_dist
-        self.state_info['direction'] = command2Vector(self.last_direction)
         self.state_info['velocity_t'] = self.v_t
         self.state_info['acceleration_t'] = self.a_t
+        self.state_info['dist_to_dest'] = self.new_dist
         self.state_info['delta_yaw_t'] = delta_yaw
         self.state_info['dyaw_dt_t'] = dyaw_dt
+        self.state_info['lateral_dist_t'] = np.linalg.norm(self.current_wpt - np.array((ego_x, ego_y)))
 
         # Update timesteps
         self.time_step += 1
@@ -162,14 +161,14 @@ class CarlaEnv(gym.Env):
         # self.last_action = self.current_action
         # print("time step %d" % self.time_step)
 
-        return (self._get_obs(), current_reward, isDone, copy.deepcopy(self.state_info))
+        return (self._info2normalized_state_vector(), current_reward, isDone, copy.deepcopy(self.state_info))
 
 
     def reset(self):
         while True:
             try:
                 # Clear sensor objects
-                self.camera_sensor = None
+                # self.camera_sensor = None
                 self.collision_sensor = None
                 self.lane_sensor = None
 
@@ -212,15 +211,14 @@ class CarlaEnv(gym.Env):
                 self.collision_hist = []
 
                 # Add camera sensor
-                self.camera_sensor = self.world.spawn_actor(self.camera_bp, self.camera_trans, attach_to=self.ego)
-
-                self.actors.append(self.camera_sensor)
-                self.camera_sensor.listen(lambda data: get_camera_img(data))
-                def get_camera_img(data):
-                    array = np.frombuffer(data.raw_data, dtype = np.dtype("uint8"))
-                    array = np.reshape(array, (data.height, data.width, 4))
-                    array = array[:, :, :3]
-                    self.camera_img = array
+                # self.camera_sensor = self.world.spawn_actor(self.camera_bp, self.camera_trans, attach_to=self.ego)
+                # self.actors.append(self.camera_sensor)
+                # self.camera_sensor.listen(lambda data: get_camera_img(data))
+                # def get_camera_img(data):
+                #     array = np.frombuffer(data.raw_data, dtype = np.dtype("uint8"))
+                #     array = np.reshape(array, (data.height, data.width, 4))
+                #     array = array[:, :, :3]
+                #     self.camera_img = array
 
                 # Add lane invasion sensor
                 self.lane_sensor = self.world.spawn_actor(self.lane_bp, carla.Transform(), attach_to=self.ego)
@@ -238,11 +236,6 @@ class CarlaEnv(gym.Env):
                 # Enable sync mode
                 self.settings.synchronous_mode = True
                 self.world.apply_settings(self.settings)
-
-                # Route teller
-                directions = self._get_directions(self.ego.get_transform(), self.dest)
-                self.last_direction = directions
-                # print("command is %s" % self.instruction[self.last_direction])
 
                 ego_x, ego_y = self._get_ego_pos()
                 dest_x, dest_y = self.dest[0], self.dest[1]
@@ -262,16 +255,15 @@ class CarlaEnv(gym.Env):
                 delta_yaw = self._get_delta_yaw()
                 dyaw_dt = self.ego.get_angular_velocity().z
 
-                # record the action of last time step
-                # self.last_action = np.array([0.0, 0.0])
-                # self.current_action = self.last_action.copy()
+                # Get waypoint infomation
+                self.current_wpt = self._get_waypoint_xy()
 
                 self.state_info['velocity_t'] = self.v_t
                 self.state_info['acceleration_t'] = self.a_t
                 self.state_info['dist_to_dest'] = self.last_dist
-                self.state_info['direction'] = command2Vector(self.last_direction)
                 self.state_info['delta_yaw_t'] = delta_yaw
                 self.state_info['dyaw_dt_t'] = dyaw_dt
+                self.state_info['lateral_dist_t'] = np.linalg.norm(self.current_wpt - np.array((ego_x, ego_y)))
 
                 # End State variable initialized
                 self.isCollided = False
@@ -280,7 +272,7 @@ class CarlaEnv(gym.Env):
                 self.isOutOfLane = False
                 self.isSpecialSpeed = False
 
-                return self._get_obs(), copy.deepcopy(self.state_info)
+                return self._info2normalized_state_vector(), copy.deepcopy(self.state_info)
             except:
                 self.logger.error("Env reset() error")
                 time.sleep(2)
@@ -301,6 +293,14 @@ class CarlaEnv(gym.Env):
         # Get ego state
         ego_x, ego_y = self._get_ego_pos()
 
+        # If at destination
+        dest = self.dest
+        if np.sqrt((ego_x-dest[0])**2+(ego_y-dest[1])**2) < 2.0:
+            # print("Get destination! Episode Done.")
+            self.logger.debug('Get destination! Episode Done.')
+            self.isSuccess = True
+            return True
+
         # If collides
         if len(self.collision_hist) > 0:
             # print("Collision happened! Episode Done.")
@@ -313,14 +313,6 @@ class CarlaEnv(gym.Env):
             # print("Time out! Episode Done.")
             self.logger.debug('Time out! Episode Done.')
             self.isTimeOut = True
-            return True
-
-        # If at destination
-        dest = self.dest
-        if np.sqrt((ego_x-dest[0])**2+(ego_y-dest[1])**2) < 2.0:
-            # print("Get destination! Episode Done.")
-            self.logger.debug('Get destination! Episode Done.')
-            self.isSuccess = True
             return True
 
         # If out of lane
@@ -337,7 +329,7 @@ class CarlaEnv(gym.Env):
             self.logger.debug("Speed too slow! Episode Done.")
             self.isSpecialSpeed = True
             return True
-        elif v_norm > 13:
+        elif v_norm > 11:
             self.logger.debug("Speed too fast! Episode Done.")
             self.isSpecialSpeed = True
             return True
@@ -420,19 +412,18 @@ class CarlaEnv(gym.Env):
             Bool indicating whether the spawn is successful.
         """
         vehicle = self.world.spawn_actor(self.ego_bp, transform)
-        self.actors.append(vehicle)
         if vehicle is not None:
-            self.ego=vehicle
+            self.actors.append(vehicle)
+            self.ego = vehicle
             return True
         return False
 
 
     def _get_obs(self):
-        current_obs = self.camera_img[36:, :, :].copy()
-        # cv2.imshow("camera img", current_obs)
-        # cv2.waitKey(1)
-        # self.world.tick()
-        return np.float32(current_obs / 255.0)
+        # TODO: switch to vector version
+        # current_obs = self.camera_img[36:, :, :].copy()
+        pass
+        # return np.float32(current_obs / 255.0)
 
 
     def _get_reward(self):
@@ -442,7 +433,9 @@ class CarlaEnv(gym.Env):
         # end state
         # reward for done: collision/out/SpecislSPeed & Success
         r_done = 0.0
+        r_step = 3.0
         if self.isCollided or self.isOutOfLane or self.isSpecialSpeed:
+            r_step = 0.0
             r_done = -300.0
         if self.isSuccess:
             r_done = 300.0
@@ -451,45 +444,21 @@ class CarlaEnv(gym.Env):
         v = self.ego.get_velocity()
         speed = np.sqrt(v.x**2 + v.y**2)
         delta_speed = speed - self.desired_speed
-        r_speed = 1.0 - delta_speed**2 / 25.0
+        r_speed = -delta_speed**2 / 9.0
         # print(r_speed)
 
         # reward for steer
         delta_yaw = self._get_delta_yaw()
-        r_steer = -10 * (delta_yaw * np.pi / 180)**2
+        r_steer = -100 * (delta_yaw * np.pi / 180)**2
+        # print(delta_yaw, '------>', r_steer)
 
         # reward for action smoothness
         current_action = self.ego.get_control()
-        self.current_action = np.array([current_action.throttle-current_action.brake, current_action.steer])
-        r_action_smooth = - np.linalg.norm(self.current_action)**2
-        # print(self.last_action, '---->', self.current_action, r_action_smooth)
+        current_action = np.array([current_action.throttle-current_action.brake, current_action.steer])
+        r_action_regularized = - 3 * np.linalg.norm(current_action)**2
+        # print(current_action, '------>', r_action_regularized)
 
-        return r_done + r_speed + r_steer + r_action_smooth
-
-
-    def _get_directions(self, current_point, end_point):
-        '''
-        params: current position; target_position
-        return: command list
-        '''
-        directions = self._planner.get_next_command(
-            (current_point.location.x,
-             current_point.location.y, 0.22),
-            (current_point.rotation.roll,
-             current_point.rotation.pitch,
-             current_point.rotation.yaw),
-            (end_point[0], end_point[1], 0.22),
-            (end_point[3], end_point[4], end_point[5]))
-        return directions
-
-
-    def _get_shortest_path(self, start_point, end_point):
-
-        return self._planner.get_shortest_path_distance(
-            [   start_point.location.x, start_point.location.y, 0.22], [
-                start_point.rotation.roll, start_point.rotation.pitch, start_point.rotation.yaw], [
-                end_point[0], end_point[1], 0.22], [
-                end_point[3], end_point[4], end_point[5]])
+        return r_done + r_speed + r_steer + r_action_regularized + r_step
 
 
     def _make_carla_client(self, host, port):
@@ -501,7 +470,6 @@ class CarlaEnv(gym.Env):
 
                 # Set map
                 self.world = self.client.load_world('Town01')
-                self._planner = Planner('Town01')
                 self.map = self.world.get_map()
 
                 # Set weather
@@ -545,3 +513,34 @@ class CarlaEnv(gym.Env):
             delta_yaw += 360
 
         return delta_yaw
+
+    def _get_waypoint_xy(self):
+        """
+        Get the (x,y) waypoint of current ego position
+            if t != 0 and None, return the wpt of last moment
+            if t == 0 and None wpt: return self.starts
+        """
+        waypoint = self.map.get_waypoint(location=self.ego.get_location())
+        if waypoint:
+            return np.array((waypoint.transform.location.x, waypoint.transform.location.y))
+        else:
+            return self.current_wpt
+
+
+    def _info2normalized_state_vector(self):
+        '''
+        params: dict of ego state(velocity_t, accelearation_t, dist, command, delta_yaw_t, dyaw_dt_t)
+        type: np.array
+        return: array of size[8,], torch.Tensor (v, a, d, delta_yaw, dyaw, d_lateral)
+        '''
+        velocity_t = self.state_info['velocity_t']
+        accel_t = self.state_info['acceleration_t']
+        dist_t = self.state_info['dist_to_dest'].reshape((1,1)) / 20.0
+        delta_yaw_t = np.array(self.state_info['delta_yaw_t']).reshape((1,1)) / 3.0
+        dyaw_dt_t = np.array(self.state_info['dyaw_dt_t']).reshape((1,1)) / 10.0
+        lateral_dist_t = self.state_info['lateral_dist_t'].reshape((1,1)) * 10.0
+
+        info_vec = np.concatenate([velocity_t, accel_t, dist_t, delta_yaw_t, dyaw_dt_t, lateral_dist_t], axis=0)
+        info_vec = info_vec.squeeze()
+
+        return  info_vec
